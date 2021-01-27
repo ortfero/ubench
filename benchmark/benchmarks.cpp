cb2b8734189803452bf04a6d38a6a673565ffc56
ortfero <ortfero@hotmail.com>
2021-01-27 16:08:10 +0300

    Update benchmark test

--- benchmark/benchmarks.cpp
+++ benchmark/benchmarks.cpp
@@ -6,23 +6,19 @@
 
 
 int main() {
-  using namespace std;
-  
-  int x = 0;
-  auto const fast_benched = ubench::run([&]{
-    ++x;
-  });
-  cout << "fast operation - " << fast_benched
-       << " [took " << fast_benched.took.count() << " us]" << endl;
-  
-  if(fast_benched.time > chrono::nanoseconds{10})
-    cout << "probably not so fast" << endl;
-  
-  auto const slow_benched = ubench::run([&]{
-    this_thread::sleep_for(chrono::milliseconds{10});
-  });
-  cout << "slow operation - " << slow_benched
-       << " [took " << slow_benched.took.count() << " us]" << endl;
+    using namespace std;
 
-  return 0;
+    int x                   = 0;
+    auto const fast_benched = ubench::run([&] { ++x; });
+    cout << "fast operation - " << fast_benched << '\n';
+
+    if(fast_benched.time > chrono::nanoseconds {10})
+        cout << "probably not so fast" << endl;
+
+    auto const slow_benched = ubench::run<10>(
+        [&] { this_thread::sleep_for(chrono::milliseconds {10}); });
+
+    cout << "slow operation - " << slow_benched << '\n';
+
+    return 0;
 }
